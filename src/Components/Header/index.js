603a4cdd73a32544d3d9a414eac0293d8ee4f49f
Igor Santana <santanna.ig@hotmail.com>
2025-03-29 23:55:40 -0300

    mudanças no header do projeto, implementando logo

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,17 +1,37 @@
-import Anuncio from './styles.ts';
+import {Anuncio, HeaderWrapper, HeaderStl, LogoLink, BotaoCountry} from './styles.ts';
+import logo from '../../assets/images/logo.png'
+import Button from '@mui/material/Button'
 
 const Header = () => {
-    return(
-        <>
-        <div className="header">
+    return (
+        <HeaderWrapper>
             <div className="top-strip">
                 <div className="container">
-                    <Anuncio>Devido a epidemia de <b>COVID 19</b>, nossas entregas podem demorar mais que o normal</Anuncio>
+                    <Anuncio>
+                        Devido a epidemia de <b>COVID 19</b>, nossas entregas podem demorar mais que o normal
+                    </Anuncio>
                 </div>
             </div>
-        </div>
-        </>
-    )
-}
+
+            <HeaderStl>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='logoWrapper col-sm-2 align-items-center'>
+                            <LogoLink to={'/'}>
+                                <img src={logo} alt="logo" />
+                            </LogoLink>
+                        </div>
+                        <BotaoCountry className='col-sm-10 d-flex align-items-center'>
+                            <Button className='countryDrop'>
+                                Sua localização
+                            </Button>
+                            
+                        </BotaoCountry>
+                    </div>
+                </div>
+            </HeaderStl>
+        </HeaderWrapper>
+    );
+};
 
 export default Header;
--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -1,11 +1,54 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 
-const Anuncio = styled.p`
-    background-color: #8e00a3;
+export const Anuncio = styled.p`
     text-align: center;
     margin-bottom: 0;
     margin-top: 0;
 `
 
-export default Anuncio;
+export const HeaderWrapper = styled.div`
+
+    .top-strip{
+        padding: 8px 0px;
+        background-color: #233a95;
+    }
+    p{
+        color: white;
+        font-size: 12px;
+        font-weight: 500;
+    }
+`
+
+export const HeaderStl = styled.div`
+    width: 100%;
+    height: auto;
+    padding: 10px 0;
+
+    .logoWrapper img{
+        width: 100%;
+    }
+    .row{
+        display: flex;
+        align-items: center;
+    }
+`
+
+export const LogoLink = styled(Link)`
+    display: inline-block;
+    transition: opacity 0.3s;
+    
+    &:hover {
+        opacity: 0.8;
+    }
+`;
+
+export const BotaoCountry = styled.div`
+
+    .countryDrop{
+        min-width: 150px;
+        width: 150px;
+        height: 80px;
+    }
+`
